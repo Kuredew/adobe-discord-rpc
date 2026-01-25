707d9361a03e69a2bb81f06ffd450cdff58c82fc
Kuredew <burrohmanhabib35@gmail.com>
2026-01-25 16:18:07 +0700

    chore: Log all copying file/folder

--- scripts/utils/buildExtension.js
+++ scripts/utils/buildExtension.js
@@ -18,16 +18,26 @@ function build(outputExtensionFolder) {
 
     log(`Copying dependencies to ${outputExtensionFolder}`)
 
+    log(`Copying assets...`)
     fs.cpSync("./src/view/assets", `${outputExtensionFolder}/view/assets`, {recursive: true})
+
+    log(`Copying panel index.html...`)
     fs.cpSync("./src/view/index.html", `${outputExtensionFolder}/view/index.html`)
+    log(`Copying extension index.html...`)
     fs.cpSync("./src/index.html", `${outputExtensionFolder}/index.html`)
 
+    log(`Copying .debug...`)
     fs.cpSync("./.debug", `${outputExtensionFolder}/.debug`)
+    log(`Copying CSXS...`)
     fs.cpSync("./CSXS", `${outputExtensionFolder}/CSXS`, {recursive: true})
+    log(`Copying libs...`)
     fs.cpSync("./libs", `${outputExtensionFolder}/libs`, {recursive: true})
+    log(`Copying jsx host...`)
     fs.cpSync("./jsx", `${outputExtensionFolder}/jsx`, {recursive: true})
 
+    log(`Copying dependency...`)
     fs.cpSync("./scripts/dep/node_modules", `${outputExtensionFolder}/node_modules`, {recursive: true})
+    log(`Copying config.json...`)
     fs.cpSync("./config.json", `${outputExtensionFolder}/config.json`)
 
     log('Successfully copied dependencies.')
