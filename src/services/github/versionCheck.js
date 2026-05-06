ca8b0b2dd44c0b956f5ea758515193677979558c
Kureichi <165907455+Kuredew@users.noreply.github.com>
2026-05-06 19:01:14 +0700

    fix: no new version detected (#46)

--- src/services/github/versionCheck.js
+++ src/services/github/versionCheck.js
@@ -1,49 +1,50 @@
 class VersionCheck {
-    constructor(manifestReader, logger) {
-        this.logger = logger
-        this.isLatestVersion = null
-        this.currentVersion = null
-        this.currentVersionStr = null
+  constructor(manifestReader, logger) {
+    this.logger = logger
+    this.isLatestVersion = null
+    this.currentVersion = null
+    this.currentVersionStr = null
 
-        this.repoUrl = "https://api.github.com/repos/Kuredew/adobe-discord-rpc"
+    this.repoUrl = "https://api.github.com/repos/Kuredew/adobe-discord-rpc"
 
-        this.manifestReader = manifestReader
+    this.manifestReader = manifestReader
+  }
+
+  async getVersion() {
+    this.manifestReader.load()
+    this.currentVersion = this.manifestReader.getManifestVersion()
+
+    this.currentVersionStr = 'v' + this.currentVersion
+
+    try {
+      this.logger.info('Checking latest version...')
+
+      const response = await fetch(`${this.repoUrl}/releases/latest`)
+      if (!response.ok) {
+        this.logger.info('Response not ok, retrying...');
+        setTimeout(() => this.check(), 3000);
+      }
+
+      this.logger.info('Response is OK')
+      const data = await response.json()
+      this.logger.info(`Latest version is ${data.tag_name} and current version is ${this.currentVersionStr}`)
+
+      const latestVersion = data.tag_name;
+      if (parseInt(latestVersion.slice(1).split('.').join('')) > parseInt(this.currentVersion.split('.').join(''))) {
+        this.logger.warn(`Version (${this.currentVersionStr}) is outdated, consider to update (to ${latestVersion})`)
+
+        this.isLatestVersion = false
+        return `New Update ${latestVersion} ↗`
+      }
+
+      this.logger.info(`This is latest version (${this.currentVersion})`)
+      this.isLatestVersion = true
+      return this.currentVersionStr
+    } catch (e) {
+      this.logger.error('Error while trying to fetch api, ' + e);
+      setTimeout(() => this.check(), 5000);
     }
-
-    async getVersion() {
-        this.manifestReader.load()
-        this.currentVersion = this.manifestReader.getManifestVersion()
-
-        this.currentVersionStr = 'v' + this.currentVersion
-
-        try {
-            this.logger.info('Checking latest version...')
-
-            const response = await fetch(`${this.repoUrl}/releases/latest`)
-            if (!response.ok) {
-                this.logger.info('Response not ok, retrying...');
-                setTimeout(() => this.check(), 3000);
-            }
-
-            this.logger.info('Response is OK')
-            const data = await response.json()
-
-            const latestVersion = data.tag_name;
-            if (parseInt(latestVersion) > parseInt(this.currentVersion)) {
-                this.logger.warn(`Version (${this.currentVersionStr}) is outdated, consider to update (to ${latestVersion})`)
-
-                this.isLatestVersion = false
-                return `New Update ${latestVersion} ↗`
-            }
-
-            this.logger.info(`This is latest version (${this.currentVersion})`)
-            this.isLatestVersion = true
-            return this.currentVersionStr
-        } catch(e) {
-            this.logger.error('Error while trying to fetch api, ' + e);
-            setTimeout(() => this.check(), 5000);
-        }
-    }
+  }
 }
 
 export default VersionCheck
